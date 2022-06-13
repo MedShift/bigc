e74a958bb9646172ce36554271077cb2f30adad6
Dillon Hartley <42848484+dhartle4@users.noreply.github.com>
2022-06-13 14:40:17 -0400

    Add support for `categories` resource (#10)

--- bigc/resources/__init__.py
+++ bigc/resources/__init__.py
@@ -1,4 +1,5 @@
 from .carts import BigCommerceCartsAPI
+from .categories import BigCommerceCategoriesAPI
 from .customers import BigCommerceCustomersAPI
 from .orders import BigCommerceOrdersAPI
 from .product_variants import BigCommerceProductVariantsAPI
---
+++ bigc/resources/categories.py
@@ -0,0 +1,33 @@
+from collections.abc import Iterator
+
+from bigc._client import BigCommerceV3APIClient
+
+
+class BigCommerceCategoriesAPI:
+    def __init__(self, store_hash: str, access_token: str):
+        self._v3_client = BigCommerceV3APIClient(store_hash, access_token)
+
+    def all(self) -> Iterator[dict]:
+        """Return an iterator for all categories"""
+        return self._v3_client.paginated_request('GET', '/catalog/categories')
+
+    def get(self, category_id: int) -> dict:
+        """Get a specific category by its ID"""
+        return self._v3_client.request('GET', f'/catalog/categories/{category_id}')
+
+    def create(self, *, name: str, is_visible: bool = True, parent_id: int = 0) -> dict:
+        """Create a category"""
+        payload = {
+            'name': name,
+            'is_visible': is_visible,
+            'parent_id': parent_id,
+        }
+        return self._v3_client.request('POST', '/catalog/categories', json=payload)
+
+    def update(self, category_id: int, data: dict) -> dict:
+        """Update a specific category by its ID"""
+        return self._v3_client.request('PUT', f'/catalog/categories/{category_id}', json=data)
+
+    def delete(self, category_id: int) -> dict:
+        """Delete a specific category by its ID"""
+        return self._v3_client.request('DELETE', f'/catalog/categories/{category_id}')
