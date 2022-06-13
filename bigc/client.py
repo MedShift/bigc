0fc0c549690fdb11ae8fd4219a0b5ef662cf7292
dillonhartley <dillon@medshift.com>
2022-06-13 14:28:32 -0400

    Add support for `categories` resource

--- bigc/client.py
+++ bigc/client.py
@@ -4,6 +4,7 @@ from bigc.resources import *
 class BigCommerceAPI:
     def __init__(self, store_hash: str, access_token: str):
         self.carts: BigCommerceCartsAPI = BigCommerceCartsAPI(store_hash, access_token)
+        self.categories: BigCommerceCategoriesAPI = BigCommerceCategoriesAPI(store_hash, access_token)
         self.customers: BigCommerceCustomersAPI = BigCommerceCustomersAPI(store_hash, access_token)
         self.orders: BigCommerceOrdersAPI = BigCommerceOrdersAPI(store_hash, access_token)
         self.products: BigCommerceProductsAPI = BigCommerceProductsAPI(store_hash, access_token)
