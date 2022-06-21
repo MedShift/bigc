5a7af3bf7840f5554ec192f1a4903ff9d73af729
Max Herold <maxoheyn@gmail.com>
2022-06-21 15:40:53 -0400

    Add support for `customer_groups` resource

--- bigc/client.py
+++ bigc/client.py
@@ -5,6 +5,7 @@ class BigCommerceAPI:
     def __init__(self, store_hash: str, access_token: str):
         self.carts: BigCommerceCartsAPI = BigCommerceCartsAPI(store_hash, access_token)
         self.categories: BigCommerceCategoriesAPI = BigCommerceCategoriesAPI(store_hash, access_token)
+        self.customer_groups: BigCommerceCustomerGroupsAPI = BigCommerceCustomerGroupsAPI(store_hash, access_token)
         self.customers: BigCommerceCustomersAPI = BigCommerceCustomersAPI(store_hash, access_token)
         self.orders: BigCommerceOrdersAPI = BigCommerceOrdersAPI(store_hash, access_token)
         self.products: BigCommerceProductsAPI = BigCommerceProductsAPI(store_hash, access_token)
