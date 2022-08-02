faf37e898c3643e19d65a940309380004d06f04d
Elijah Horton <elijah@medshift.com>
2022-08-02 14:58:22 -0400

    Refactor pricing arguments

--- bigc/resources/pricing.py
+++ bigc/resources/pricing.py
@@ -7,12 +7,12 @@ class BigCommercePricingAPI:
     def __init__(self, store_hash: str, access_token: str):
         self._v3_client = BigCommerceV3APIClient(store_hash, access_token)
 
-    def products(self, customer_group_id: int, items: list[dict], channel_id: int = 1,
-                 currency_code: str = 'USD') -> Iterator[dict]:
+    def products(self, items: list[dict], *, channel_id: int = 1, currency_code: str = 'USD',
+                 customer_group_id: int = 0) -> Iterator[dict]:
         """Return an iterator for batch product pricing"""
         return self._v3_client.paginated_request('POST', '/pricing/products', json={
-            'customer_group_id': customer_group_id,
-            'items': items,
             'channel_id': channel_id,
             'currency_code': currency_code,
+            'customer_group_id': customer_group_id,
+            'items': list(items),
         })
