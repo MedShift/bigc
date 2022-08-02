d56636024420fabd1b46c0190ed63cf7da64adef
Elijah Horton <elijah@medshift.com>
2022-08-02 16:56:46 -0400

    Refactor batch pricing and put on BCProductsAPI module

--- bigc/resources/__init__.py
+++ bigc/resources/__init__.py
@@ -3,7 +3,6 @@ from .categories import BigCommerceCategoriesAPI
 from .customer_groups import BigCommerceCustomerGroupsAPI
 from .customers import BigCommerceCustomersAPI
 from .orders import BigCommerceOrdersAPI
-from .pricing import BigCommercePricingAPI
 from .product_variants import BigCommerceProductVariantsAPI
 from .products import BigCommerceProductsAPI
 from .webhooks import BigCommerceWebhooksAPI
--- bigc/resources/products.py
+++ bigc/resources/products.py
@@ -60,3 +60,13 @@ class BigCommerceProductsAPI:
     def delete(self, product_id: int) -> dict:
         """Delete a specific product by its ID"""
         return self._v3_client.request('DELETE', f'/catalog/products/{product_id}')
+
+    def get_pricing(self, items: list[dict], *, channel_id: int = 1, currency_code: str = 'USD',
+                    customer_group_id: int = 0) -> Iterator[dict]:
+        """Return an iterator for batch product pricing"""
+        return self._v3_client.paginated_request('POST', '/pricing/products', json={
+            'channel_id': channel_id,
+            'currency_code': currency_code,
+            'customer_group_id': customer_group_id,
+            'items': list(items),
+        })
