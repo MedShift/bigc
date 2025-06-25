b8a7ca5424163fd32a1c9919c522aa907ecf89c6
Milo Weinberg <38477514+Iapetus-11@users.noreply.github.com>
2025-06-25 07:53:16 -0400

    Reorganize Resources (#54)

--- bigc/resources/checkouts_v3.py
+++ bigc/resources/checkouts_v3.py
@@ -1,14 +1,14 @@
 from typing import Any, TypeAlias
 from uuid import UUID
 
-from bigc.api_client import BigCommerceAPIClient
+from bigc.api_client import BigCommerceV3APIClient
 
 UUIDLike: TypeAlias = UUID | str
 
 
-class BigCommerceCheckoutsAPI:
-    def __init__(self, api_client: BigCommerceAPIClient):
-        self._api = api_client
+class BigCommerceCheckoutsV3API:
+    def __init__(self, api: BigCommerceV3APIClient):
+        self._api = api
 
     def get(
             self,
@@ -18,11 +18,11 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Get a specific checkout by its ID"""
-        return self._api.v3.get(f'/checkouts/{checkout_id}', params=params, timeout=timeout)
+        return self._api.get(f'/checkouts/{checkout_id}', params=params, timeout=timeout)
 
     def update(self, checkout_id: UUIDLike, data: dict[str, Any], *, timeout: float | None = None) -> dict[str, Any]:
         """Change customer message pertaining to an existing Checkout"""
-        return self._api.v3.put(f'/checkouts/{checkout_id}', data=data, timeout=timeout)
+        return self._api.put(f'/checkouts/{checkout_id}', data=data, timeout=timeout)
 
     def add_billing_address(
             self,
@@ -32,7 +32,7 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Add a billing address to an existing checkout"""
-        return self._api.v3.post(f'/checkouts/{checkout_id}/billing-address', data=data, timeout=timeout)
+        return self._api.post(f'/checkouts/{checkout_id}/billing-address', data=data, timeout=timeout)
 
     def update_billing_address(
             self,
@@ -42,7 +42,7 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Update an existing billing address on a checkout"""
-        return self._api.v3.put(f'/checkouts/{checkout_id}/billing-address/{address_id}', data=data, timeout=timeout)
+        return self._api.put(f'/checkouts/{checkout_id}/billing-address/{address_id}', data=data, timeout=timeout)
 
     def add_consignment(
             self,
@@ -51,7 +51,7 @@ class BigCommerceCheckoutsAPI:
             params: dict[str, Any] | None = None, timeout: float | None = None,
     ) -> dict[str, Any]:
         """Add a new consignment to a checkout"""
-        return self._api.v3.post(f'/checkouts/{checkout_id}/consignments', data=data, params=params, timeout=timeout)
+        return self._api.post(f'/checkouts/{checkout_id}/consignments', data=data, params=params, timeout=timeout)
 
     def update_consignment(
             self,
@@ -61,7 +61,7 @@ class BigCommerceCheckoutsAPI:
             params: dict[str, Any] | None = None, timeout: float | None = None,
     ) -> dict[str, Any]:
         """Update an existing consignment's selected shipping option"""
-        return self._api.v3.put(f'/checkouts/{checkout_id}/consignments/{consignment_id}', data=data, params=params, timeout=timeout)
+        return self._api.put(f'/checkouts/{checkout_id}/consignments/{consignment_id}', data=data, params=params, timeout=timeout)
 
     def delete_consignment(
             self,
@@ -72,7 +72,7 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Remove an existing consignment from a checkout"""
-        return self._api.v3.delete(f'/checkouts/{checkout_id}/consignments/{consignment_id}', params=params, timeout=timeout)
+        return self._api.delete(f'/checkouts/{checkout_id}/consignments/{consignment_id}', params=params, timeout=timeout)
 
     def add_coupon(
             self,
@@ -83,7 +83,7 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Add a coupon code to a checkout"""
-        return self._api.v3.post(f'/checkouts/{checkout_id}/coupons', data=data, params=params, timeout=timeout)
+        return self._api.post(f'/checkouts/{checkout_id}/coupons', data=data, params=params, timeout=timeout)
 
     def delete_coupon(
             self,
@@ -94,7 +94,7 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Delete a coupon code from a checkout"""
-        return self._api.v3.delete(f'/checkouts/{checkout_id}/coupons/{coupon_code}', params=params, timeout=timeout)
+        return self._api.delete(f'/checkouts/{checkout_id}/coupons/{coupon_code}', params=params, timeout=timeout)
 
     def add_discounts(
             self,
@@ -105,7 +105,7 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Add discounts to an existing checkout"""
-        return self._api.v3.post(f'/checkouts/{checkout_id}/discounts', data=data, params=params, timeout=timeout)
+        return self._api.post(f'/checkouts/{checkout_id}/discounts', data=data, params=params, timeout=timeout)
 
     def create_order(
             self,
@@ -115,4 +115,4 @@ class BigCommerceCheckoutsAPI:
             timeout: float | None = None,
     ) -> dict[str, Any]:
         """Create an order"""
-        return self._api.v3.post(f'/checkouts/{checkout_id}/orders', params=params, timeout=timeout)
+        return self._api.post(f'/checkouts/{checkout_id}/orders', params=params, timeout=timeout)
--- bigc/resources/currencies_v2.py
+++ bigc/resources/currencies_v2.py
@@ -1,16 +1,16 @@
 from typing import Any, Iterator
 
-from bigc.api_client import BigCommerceAPIClient
+from bigc.api_client import BigCommerceV2APIClient
 
 
-class BigCommerceCurrenciesAPI:
-    def __init__(self, api_client: BigCommerceAPIClient):
-        self.api = api_client
+class BigCommerceCurrenciesV2API:
+    def __init__(self, api: BigCommerceV2APIClient):
+        self._api = api
 
     def all(self, *, params: dict[str, Any] | None = None, timeout: float | None = None) -> Iterator[dict[str, Any]]:
         """Return an iterator for all currencies"""
-        return self.api.v2.get_many('/currencies', params=params, timeout=timeout)
+        return self._api.get_many('/currencies', params=params, timeout=timeout)
 
     def get(self, currency_id: int, *, params: dict[str, Any] | None = None, timeout: float | None = None):
         """Get a specific currency by its ID"""
-        return self.api.v2.get(f'/currencies/{currency_id}', params=params, timeout=timeout)
+        return self._api.get(f'/currencies/{currency_id}', params=params, timeout=timeout)
